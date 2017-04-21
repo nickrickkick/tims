08fa0def2267546df6fc5b23d38469ed51e15aca
Tanis <jeditanis@gmail.com>
2017-04-21 15:17:55 -0600

    requiring commit

--- tims/animation.js
+++ tims/animation.js
@@ -1,58 +1,10 @@
-var P1Walking = new Image(); //provides variable to assign the animated image
-P1Walking.src = "images/BlueTIMSMap" // assigns the animated image
-var canvas = document.getElementById("P1Walking");
-canvas.width = 32;
-canvas.height = 32;
-var P1Walk = P1sprite({
-    context: canvas.getContect("2d"),
-    width: 32,
-    hight 32,
-    image: P1Walking
-});
-
-
-// allows calling of multiple sprites 
-function P1sprite (options) {
-    var that = {},
-        frameIndex = 0;
-        tickCount = 0,
-        ticksPerFrame = options.ticksPerFrame || 1;
-                
-    that.context = options.context;
-    that.width = options.width
-    that.height = options.height;
-    that.image = options.image;
-    
-    return that;
-    
-    that.render = function () {
-        //draw the animation
-             that.context.drawImage(
-           that.image,
-           frameIndex * that.width / numberOfFrames,
-           0,
-           that.width / numberOfFrames,
-           that.height,
-           0,
-           0,
-           that.width / numberOfFrames,
-           that.height);
-          that.update = function () {
-              
-        tickCount += 1;
-			
-        if (tickCount > ticksPerFrame) {
-        
-        	tickCount = 0;
-        	
-                // If the current frame index is in range
-            if (frameIndex < numberOfFrames - 1) {	
-            // Go to the next frame
-            frameIndex += 1;  
-        }
-    };
-}
-}
-}
-
-
+function Sprite(url, pos, size, speed, frames, dir, once) {
+    this.pos = pos;
+    this.size = size;
+    this.speed = typeof speed === 'number' ? speed : 0;
+    this.frames = frames;
+    this._index = 0;
+    this.url = url;
+    this.dir = dir || 'horizontal';
+    this.once = once;
+};
