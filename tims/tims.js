6d5ffcd71fa744d80ae0d39d0307f9d69da71669
michael nowlin <michaelnowlin5@gmail.com>
2017-04-21 16:21:55 +0000

    more var

--- tims/tims.js
+++ tims/tims.js
@@ -1,5 +1,10 @@
 // variables that could be usefull
+// fight global
 var PlayerHealth = 10;
 var PlayerMovementSpeeed = 5;
 var PlayerDmg = 4; // damage that player outputs
 var Score = 0; //score system
+//maps global
+var people = 5;
+var map;
+var movement= 3;
