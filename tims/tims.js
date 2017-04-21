2444b30a34c88f1a7f412f165704a530b815f74a
michael nowlin <michaelnowlin5@gmail.com>
2017-04-21 18:35:27 +0000

    music

--- tims/tims.js
+++ tims/tims.js
@@ -2,12 +2,12 @@
 // fight global
 var PlayerHealth = 10;
 var PlayerMovementSpeeed = 5;
-<<<<<<< HEAD
 var PlayerDmg = Math.floor(Math.random() * 6); // damage that player outputs
-var Score = 0; //score system
+var battle_music= document.getElementById("battle_music");
 //maps global
 var people = 5;
 var map;
 var movement= 3;
 var Player1;
 var Player2;
+var map_music= document.getElementById("map_music");
