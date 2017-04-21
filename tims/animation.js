96b37b4526040070571566245b87f96dbe508117
Tanis <jeditanis@gmail.com>
2017-04-21 15:16:09 -0600

    rewritting animation.js

--- tims/animation.js
+++ tims/animation.js
@@ -2,7 +2,7 @@ var P1Walking = new Image(); //provides variable to assign the animated image
 P1Walking.src = "images/BlueTIMSMap" // assigns the animated image
 var canvas = document.getElementById("P1Walking");
 canvas.width = 32;
-canvas.hight = 32;
+canvas.height = 32;
 var P1Walk = P1sprite({
     context: canvas.getContect("2d"),
     width: 32,
@@ -16,7 +16,7 @@ function P1sprite (options) {
     var that = {},
         frameIndex = 0;
         tickCount = 0,
-        ticksPerFrame = options.ticksPerFrame || 0;
+        ticksPerFrame = options.ticksPerFrame || 1;
                 
     that.context = options.context;
     that.width = options.width
@@ -45,11 +45,14 @@ function P1sprite (options) {
         
         	tickCount = 0;
         	
+                // If the current frame index is in range
+            if (frameIndex < numberOfFrames - 1) {	
             // Go to the next frame
             frameIndex += 1;  
         }
     };
 }
-        
+}
+}
 
 
