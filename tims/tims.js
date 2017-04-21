27d3ed080ed8fdf5b451bf2b0309c514548b0a23
michael nowlin <michaelnowlin5@gmail.com>
2017-04-21 19:39:51 +0000

    images

--- tims/tims.js
+++ tims/tims.js
@@ -6,6 +6,7 @@ var PlayerHealth = 10;
 var PlayerMovementSpeeed = 5;
 var PlayerDmg = Math.floor(Math.random() * 6); // damage that player outputs
 var battle_music= document.getElementById("battle_music");
+var battle;
 //maps global
 var people= 4;
 var map;
@@ -13,3 +14,18 @@ var movement= 3;
 var blueplay;
 var redplay;
 var map_music= document.getElementById("map_music");
+
+
+
+function draw(){
+       context.save();
+     
+      map = new Image;
+    map.src = "map.png";
+    context.drawImage(map, x, 0);
+ context.drawImage(map, map.width + x, 0);
+ 
+ battle = new Image;
+ map.src = "battle.png";
+
+}
