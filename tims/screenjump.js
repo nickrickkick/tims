01e1f4a13d51a8652bba8c0a9b89bfece3a87983
michael nowlin <michaelnowlin5@gmail.com>
2017-04-22 02:01:51 +0000

    help

--- tims/screenjump.js
+++ tims/screenjump.js
@@ -1,13 +1,15 @@
 //on shield press, change from homescreen to Map
-var press = document.getElementsByClassName("start");
+
+
+(function() {
+      var press = document.getElementsByClassName("start");
 var canvas= document.getElementById("land");
-console.log(press);
+//console.log(press);
     press.item(0).onclick = function() {
     console.log("hello");
     document.getElementById("canvas_wrap").style.backgroundImage = "url('images/MapTIMS.png')";
-    
-    
-}
+    }   
+})();
 
 //on character collision, change from map to battlefield
 
