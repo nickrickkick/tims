b634085c360c65972d103d90f312d16225843100
Tanis <jeditanis@gmail.com>
2017-04-21 20:13:31 -0600

    another animation overhaul

--- tims/animation.js
+++ tims/animation.js
@@ -1,60 +1,76 @@
-Sprite("images/RedTIMSMap.png", "0, 0", "32px, 32px", "60", [0, 1, 2, 3], "horizontal", false)
-function Sprite(url, pos, size, speed, frames, dir, once) {
-        this.pos = pos;
-        this.size = size;
-        this.speed = typeof speed === 'number' ? speed : 0;
-        this.frames = frames;
-        this._index = 0;
-        this.url = url;
-        this.dir = dir || 'horizontal';
-        this.once = once;
-    };
-
-
-var loadAnimation = function() {
-
-    Sprite.prototype = {
-        update: function(dt) {
-            this._index += this.speed*dt;
-        },
-
-        render: function(ctx) {
-            var frame;
-
-            if(this.speed > 0) {
-                var max = this.frames.length;
-                var idx = Math.floor(this._index);
-                frame = this.frames[idx % max];
-
-                if(this.once && idx >= max) {
-                    this.done = true;
-                    return;
-                }
-            }
-            else {
-                frame = 0;
-            }
-
-
-            var x = this.pos[0];
-            var y = this.pos[1];
-
-            if(this.dir == 'vertical') {
-                y += frame * this.size[1];
-            }
-            else {
-                x += frame * this.size[0];
-            }
-
-            ctx.drawImage(resources.get(this.url),
-                          0, 0,
-                          this.size[0], this.size[1],
-                          0, 0,
-                          this.size[0], this.size[1]);
-        }
-    };
-
-    window.player2 = Sprite;
-};loadAnimation();
+ //Width and height for our canvas
+ var canvasWidth = 1000; 
+ var canvasHeight = 750;
  
-  
+ //the with and height of our spritesheet
+ var spriteWidth = 128; 
+ var spriteHeight = 32; 
+ 
+ //we are having two rows and 8 cols in the current sprite sheet
+ var rows = 1; 
+ var cols = 4; 
+ 
+ //The 0th (first) row is for the right movement
+ var trackRight = 0; 
+ 
+ //1st (second) row for the left movement (counting the index from 0)
+ var trackLeft = 1; 
+ 
+ //To get the width of a single sprite we divided the width of sprite with the number of cols
+ //because all the sprites are of equal width and height 
+ var width = 32; 
+ 
+ //Same for the height we divided the height with number of rows 
+ var height = 32; 
+ 
+ //Each row contains 4 frame and at start we will display the first frame (assuming the index from 0)
+ var curFrame = 0; 
+ 
+ //The total frame is 4
+ var frameCount = 4; 
+ 
+ //x and y coordinates to render the sprite 
+ var x=0;
+ var y=0; 
+ 
+ //x and y coordinates of the canvas to get the single frame 
+ var srcX=0; 
+ var srcY=0; 
+ 
+ //tracking the movement left and write 
+ var left = false;
+ 
+                        //Assuming that at start the character will move right side 
+ var right = false;
+ 
+ //Speed of the movement 
+ var speed = 12; 
+ 
+ //Getting the canvas 
+ var canvas = document.getElementById('canvas_wrap');
+ 
+ //Creating an Image object for our character 
+ var character = new Image(); 
+ 
+ //Setting the source to the image file 
+ character.src = "RedTIMSMap.png";
+
+function updateFrame(){
+ //Updating the frame index 
+ curFrame = ++curFrame % frameCount; 
+ 
+ //Calculating the x coordinate for spritesheet 
+ srcX = curFrame * width; 
+}
+
+var ctx = document.getElementById('canvas_wrap').getContext('2d');
+
+function draw(){
+ //Updating the frame 
+ updateFrame();
+ //Drawing the image 
+ ctx.drawImage(character,srcX,srcY,width,height,x,y,width,height);
+ ctx.clearRect(x,y,width,height);
+}
+
+setInterval(draw,100);
