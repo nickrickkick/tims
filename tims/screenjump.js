42664ea527f29fa1659db6b0d8b71b22c75e817c
michael nowlin <michaelnowlin5@gmail.com>
2017-04-22 01:57:37 +0000

    helllo

--- tims/screenjump.js
+++ tims/screenjump.js
@@ -1,6 +1,7 @@
 //on shield press, change from homescreen to Map
 var press = document.getElementsByClassName("start");
 var canvas= document.getElementById("land");
+console.log(press);
     press.item(0).onclick = function() {
     console.log("hello");
     document.getElementById("canvas_wrap").style.backgroundImage = "url('images/MapTIMS.png')";
