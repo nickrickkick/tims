67b0d09a3d8ee2943ecdeaaa1beb34ee23ff340e
michael nowlin <michaelnowlin5@gmail.com>
2017-04-22 02:16:02 +0000

    help

--- tims/screenjump.js
+++ tims/screenjump.js
@@ -7,7 +7,7 @@ var canvas= document.getElementById("land");
     press.item(0).onclick = function() {
     console.log("hello");
     document.getElementById("canvas_wrap").style.backgroundImage = "url('images/MapTIMS.png')";
-    press.style.zIndex = "1";
+    press.style.zIndex = "2";
     }   
 }};
 
