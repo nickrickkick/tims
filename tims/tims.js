a58b59e7724148fb1d739d5c7f1fe987eb9ab037
Tanis <jeditanis@gmail.com>
2017-04-22 05:06:41 -0600

    added ;ladsfja;sl

--- tims/tims.js
+++ tims/tims.js
@@ -28,17 +28,17 @@ var player2Health = 10;
 
  
        
- 
+
 function keyPressBlue(e){
     if(e.keyCode == 32) {
-         document.getElementsByClassName("blueFight").style.backgroundImage = "url('images/BlueBoard.gif')"
+        document.getElementsById("blueFight").style.backgroundImage = "url('images/BlueBoard.gif')"
      }
 }
 function keyReleaseBlue(e) {
     if(e.keyCode == 32) {
-         document.getElementsByClassName("blueFight").style.backgroundImage = "url('images/BlueBoardSolo.png')"
+         document.getElementsById("blueFight").style.backgroundImage = "url('images/BlueBoardSolo.png')"
      }
- }
+}
    
 function keyPressBlue(e){
    
@@ -84,7 +84,6 @@ function keyPressBlue(e){
     if(e.keyCode == 87){
         horizontal = -10;
     move_y = horizontal;
-    console.log("hello");
     document.getElementById("blueFight").style.transform = "rotate(360deg)";
     }
     moveBlue();
